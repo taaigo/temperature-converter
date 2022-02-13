bd410a1474faea7569689963f4aed8a5cc26337b
taigochan <taigo5@yahoo.com>
2022-02-14 00:44:26 +0900

    added kelvin

--- main.cpp
+++ main.cpp
@@ -27,11 +27,62 @@ void FC()
 	cout << FCf << "F = " << FCc << "C" << endl;
 }
 
+void CK()
+{
+	float CKc;
+
+	cout << "number: ";
+	cin >> CKc;
+
+	float CKk = CKc + 273;
+
+	cout << CKc << "C = " << CKk << "K" << endl;
+}
+
+void KC()
+{
+	float KCk;
+
+	cout << "number: ";
+	cin >> KCk;
+
+	float KCc = KCk - 273;
+
+	cout << KCk << "K = " << KCc << "C" << endl;
+}
+
+void FK()
+{
+	float FKf;
+
+	cout << "number: ";
+	cin >> FKf;
+
+	float FKc = (FKf - 32) * 5 / 9;
+	float FKk = FKc + 273;
+
+	cout << FKf << "F = " << FKk << "K" << endl;
+}
+
+void KF()
+{
+	float KFk;
+
+	cout << "number: ";
+	cin >> KFk;
+
+	float KFc = KFk - 273;
+	float KFf = (KFc * 9 / 5) + 35;
+
+	cout << KFk << "K = " << KFf << "F" << endl;
+}
+
+
 int main()
 {
 	int direction;
 
-	cout << "1. C to F" << endl << "2. F to C" << endl << "choice: ";
+	cout << "1. Celcius to Fahrenheit" << endl << "2. Fahrenheit to Celcius" << endl << "3. Celcius to Kelvin" << endl << "4. Kelvin to Celcius" << endl << "5. Fahrenheit to Kelvin" << endl << "choice: ";
 	cin >> direction;
 
 	switch (direction)
@@ -44,6 +95,26 @@ int main()
 		case 2:
 		{
 			FC();
+			break;
+		}
+		case 3:
+		{
+			CK();
+			break;
+		}
+		case 4:
+		{
+			KC();
+			break;
+		}
+		case 5:
+		{
+			FK();
+			break;
+		}
+		case 6:
+		{
+			KF();
 			break;
 		}
 		default:
